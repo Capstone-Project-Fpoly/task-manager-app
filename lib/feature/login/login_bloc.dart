c571c531f229d4551d6513290903902c73a79b56
Thanh-PH20122 <thanhdvph20122@pft.edu.vn>
2024-01-09 11:45:58 +0700

    login by google

--- lib/feature/login/login_bloc.dart
+++ lib/feature/login/login_bloc.dart
@@ -1,7 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:task_manager/base/bloc/bloc_base.dart';
 import 'package:task_manager/base/dependency/app_service.dart';
-import 'package:task_manager/graphql/queries/getAllBooks.graphql.dart';
 
 class LoginBloc extends BlocBase {
   final Ref ref;
@@ -12,8 +13,24 @@ class LoginBloc extends BlocBase {
   }
 
   Future<void> init() async {
-    final result = await graphqlService.client.query$getAllBooks(
-      Options$Query$getAllBooks(),
+    // final result = await graphqlService.client.query$getAllBooks(
+    //   Options$Query$getAllBooks(),
+    // );
+  }
+
+  Future<void> onTapLoginByGoogle() async {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+    final GoogleSignInAccount? googleSignInAccount =
+        await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount!.authentication;
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
     );
+    final UserCredential userCredential =
+        await _auth.signInWithCredential(credential);
+    print(userCredential);
   }
 }
--- lib/feature/login/login_screen.dart
+++ lib/feature/login/login_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:task_manager/base/bloc/bloc_provider.dart';
 import 'package:task_manager/base/dependency/app_service.dart';
-import 'package:task_manager/base/dependency/router/utils/route_input.dart';
+import 'package:task_manager/constants/colors.dart';
 
 class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
@@ -10,9 +10,14 @@ class LoginScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final bloc = ref.watch(BlocProvider.login);
     final routeService = ref.watch(AppService.router);
-    return TextButton(
-      child: const Text('màn login'),
-      onPressed: () => routeService.push(RouteInput.root()),
+    return Scaffold(
+      backgroundColor: ColorConstants.white,
+      body: Center(
+        child: TextButton(
+          child: const Text('Login google'),
+          onPressed: () => bloc.onTapLoginByGoogle(),
+        ),
+      ),
     );
   }
 }
